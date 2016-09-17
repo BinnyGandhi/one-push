134dd96bc72c25afe9e835ccb5f29816134f30fb
BinnyGandhi <bkgandhi27296@gmail.com>
2016-09-17 20:01:40 +0530

    Add search functionality and fixed liked functionality

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ var app = {};
 
 $(document).ready(function () {
     app.template = Handlebars.compile(document.getElementById('handlebar-template').innerHTML);
+    if (!localStorage.getItem('likes')) {
+        localStorage.setItem("likes", "{}");
+    }
     fetchData();
 
     $('#addModal').on('hidden.bs.modal', function () {
         var alert = $('#addModalAlert');
         alert.addClass('sr-only');
         alert.html('');
+        $('#add-title').html('');
+        $('#add-tag').html('');
+        $('#add-url-address').html('');
     });
 });
 
@@ -22,6 +28,16 @@ function fetchData() {
         type: 'json',
         crossOrigin: true,
         success: function (response) {
+            var lengthWebsites = response.websites.length;
+            var likes = JSON.parse(localStorage.getItem("likes"));
+            for (var i = countKeys(likes); i < lengthWebsites; i++) {
+                likes[response.websites[i].id] = 0;
+            }
+
+            for (var i in response.websites) {
+                response.websites[i].likes = likes[response.websites[i].id];
+            }
+
             app.message = response;
             displayLinks(app.message);
         },
@@ -32,8 +48,23 @@ function fetchData() {
     });
 }
 
+function incrementLikes(id) {
+    var likes = JSON.parse(localStorage.getItem("likes"));
+
+    for (var i = 0; i < app.message.websites.length; i++) {
+        if (app.message.websites[i].id == id) {
+            app.message.websites[i].likes += 1;
+            likes[id] = app.message.websites[i].likes;
+            break;
+        }
+    }
+
+    localStorage.setItem("likes", JSON.stringify(likes));
+    displayLinks(app.message);
+}
 
 function displayLinks(websites) {
+    document.getElementById('list-all-data').innerHTML = '';
     var templateData = app.template(websites);
     document.getElementById('list-all-data').innerHTML = templateData;
 }
@@ -52,7 +83,6 @@ function contributeWebsite() {
         success: function (response) {
             if (response.status == "200") {
                 $('#addModal').modal('hide');
-                document.getElementById('list-all-data').innerHTML = '';
                 fetchData();
             } else {
                 var alert = $('#addModalAlert');
@@ -66,6 +96,27 @@ function contributeWebsite() {
             console.log(JSON.stringify(error, null, 2));
         }
     });
+}
 
+function search() {
+    var query = document.getElementById('searchWebsite').value.trim();
 
+    var results = [];
+    var websites = app.message.websites;
+
+    for (var i = 0; i < websites.length; i++) {
+        if (websites[i].title.startsWith(query) || websites[i].tag.startsWith(query) || websites[i].url_address.startsWith(query)) {
+            results.push(websites[i]);
+        }
+    }
+
+    displayLinks({websites: results});
+}
+
+function countKeys(obj) {
+    var counter = 0;
+    for (var key in obj) {
+        counter++;
+    }
+    return counter;
 }
